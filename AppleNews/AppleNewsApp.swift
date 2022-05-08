cc112bc95769fcf5ddc945829701ae212ec1883b
ShinhyukPark <parkgodhyuk@gmail.com>
2022-05-08 16:33:33 +0900

    Initial Commit

---
+++ AppleNews/AppleNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleNewsApp.swift
+//  AppleNews
+//
+//  Created by Shinhyuk on 2022/05/08.
+//
+
+import SwiftUI
+
+@main
+struct AppleNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
